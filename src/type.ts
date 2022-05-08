a98929769bd330d991c3aecfad56d7e35a2bee28
XueMei-L <alu0101225845@ull.edu.es>
2022-05-08 20:25:45 +0200

    create type.ts for resquestType and RespondeType

---
+++ src/type.ts
@@ -0,0 +1,23 @@
+export type note = {
+    user?: string,
+    title?: string,
+    body?: string,
+    color?: string,
+  }
+  
+  export type RequestType = {
+    type: 'add' | 'update' | 'remove' | 'read' | 'list';
+    user?: string;
+    title?: string;
+    newtitle?: string;
+    body?: string;
+    color?: string;
+  }
+
+  
+  export type ResponseType = {
+    type: 'add' | 'update' | 'remove' | 'read' | 'list';
+    success: boolean;
+    message: [string[], string?]
+    notes?: note[];
+  }
