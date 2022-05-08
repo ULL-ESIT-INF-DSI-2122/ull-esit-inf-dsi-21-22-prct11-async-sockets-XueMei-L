0781b27793336fa54de15be6ebe2283a8c1d31c5
XueMei-L <alu0101225845@ull.edu.es>
2022-05-08 20:28:52 +0200

    added test for eventEmiiterServer

--- tests/eventEmitterServer.spec.ts
+++ tests/eventEmitterServer.spec.ts
@@ -6,45 +6,45 @@ import {RequestType} from '../src/type';
 
 
 describe('Prueba de EventEmitterServer', () => {
-    it('01 - Test for eventEmitterServer', (done) => {
-      const socket = new EventEmitter();
-      const server = new MessageEventEmitterServer(socket);
+  it('01 - Test for eventEmitterServer', (done) => {
+    const socket = new EventEmitter();
+    const server = new MessageEventEmitterServer(socket);
 
-      server.on('request', (solicitud: RequestType) => {
-        expect(solicitud.type).to.be.equal("add");
-        expect(solicitud.user).to.be.equal("alu");
-        expect(solicitud.title).to.be.equal("This is a test");
-        expect(solicitud.body).to.be.equal("Test");
-        expect(solicitud.color).to.be.equal("blue");
-        done();
-      });
-  
-      socket.emit('data', '{"type": "add",');
-      socket.emit('data', ' "user": "alu", ');
-      socket.emit('data', `"title": "This is a test", `);
-      socket.emit('data', `"body": "Test"`);
-      socket.emit('data', `, "color": "blue"}\n`);;
-      socket.emit('end');
+    server.on('request', (solicitud: RequestType) => {
+      expect(solicitud.type).to.be.equal("add");
+      expect(solicitud.user).to.be.equal("alu");
+      expect(solicitud.title).to.be.equal("This is a test");
+      expect(solicitud.body).to.be.equal("Test");
+      expect(solicitud.color).to.be.equal("blue");
+      done();
     });
 
-    it('02 - Test for eventEmitterServer', (done) => {
-        const socket = new EventEmitter();
-        const server = new MessageEventEmitterServer(socket);
-  
-        server.on('request', (solicitud: RequestType) => {
-          expect(solicitud.type).to.be.equal("read");
-          expect(solicitud.user).to.be.equal("alu");
-          expect(solicitud.title).to.be.equal("This is test02");
-          expect(solicitud.body).to.be.equal("Test");
-          expect(solicitud.color).to.be.equal("green");
-          done();
-        });
-    
-        socket.emit('data', '{"type": "read",');
-        socket.emit('data', ' "user": "alu", ');
-        socket.emit('data', `"title": "This is test02", `);
-        socket.emit('data', `"body": "Test"`);
-        socket.emit('data', `, "color": "green"}\n`);;
-        socket.emit('end');
-      });
+    socket.emit('data', '{"type": "add",');
+    socket.emit('data', ' "user": "alu", ');
+    socket.emit('data', `"title": "This is a test", `);
+    socket.emit('data', `"body": "Test"`);
+    socket.emit('data', `, "color": "blue"}\n`); ;
+    socket.emit('end');
   });
+
+  it('02 - Test for eventEmitterServer', (done) => {
+    const socket = new EventEmitter();
+    const server = new MessageEventEmitterServer(socket);
+
+    server.on('request', (solicitud: RequestType) => {
+      expect(solicitud.type).to.be.equal("read");
+      expect(solicitud.user).to.be.equal("alu");
+      expect(solicitud.title).to.be.equal("This is test02");
+      expect(solicitud.body).to.be.equal("Test");
+      expect(solicitud.color).to.be.equal("green");
+      done();
+    });
+
+    socket.emit('data', '{"type": "read",');
+    socket.emit('data', ' "user": "alu", ');
+    socket.emit('data', `"title": "This is test02", `);
+    socket.emit('data', `"body": "Test"`);
+    socket.emit('data', `, "color": "green"}\n`); ;
+    socket.emit('end');
+  });
+});
